2f5ceffa70cb14c48d78b84db198a2282d161aba
OsmanliFarid <osmanliferid686@gmail.com>
2025-02-27 21:51:22 +0400

    javascript2 task

---
+++ javascript/app.js
@@ -0,0 +1,12 @@
+let num = +prompt("bir reem yaz")
+
+if(num < 10){
+    console.log("2 reqemli eded yazin");
+    
+}else{
+    while(num < 100){
+        num += 7
+        console.log(num)
+    }
+}
+
