e8293b0703fb87dd76ffb2212df5f95fd5c05c0f
OsmanliFarid <osmanliferid686@gmail.com>
2025-02-28 09:55:30 +0400

    javascript task create

--- javascript6/app.js
+++ javascript6/app.js
@@ -1,1 +1,10 @@
+let num = +prompt("reqem daxil edin")
+let e = 0
 
+    for(let i = 100; i < 999; i++){
+        if(i % num === 0){
+            e += i
+            console.log(i)
+        }
+    }
+console.log(`${num} boluneni olan 3 reqemli ededlerinin cemidir -- ${e}`);
